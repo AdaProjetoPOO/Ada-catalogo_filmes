c4d49665e2c599525e22bcfa40304c31fa3b11c1
Leonardo7Silva <leo7kisam@gmail.com>
2023-08-17 14:50:27 -0300

    Enum/Pessoa/Diretor/Ator - Entities

---
+++ src/br/com/ada/catalogoFilmes/enuns/Sexo.java
@@ -0,0 +1,19 @@
+package br.com.ada.catalogoFilmes.enuns;
+
+public enum Sexo {
+	
+	M("Masculino"), 
+	F("Feminino"), 
+	N("Neutro");
+
+	final private String nome;
+
+	Sexo(String nome) {
+	        this.nome = nome;
+	    }
+
+	public String getNome() {
+		return nome;
+	}
+
+}
