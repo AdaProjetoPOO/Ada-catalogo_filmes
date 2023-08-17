173600398238a71e52fe70aa97c68ae6a70c1c99
Victor S. Camargo <victorscamargo51@gmail.com>
2023-08-17 20:49:18 -0300

    Repository

--- src/br/com/ada/catalogoFilmes/repository/AtorRepository.java
+++ src/br/com/ada/catalogoFilmes/repository/AtorRepository.java
@@ -1,5 +1,41 @@
 package br.com.ada.catalogoFilmes.repository;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import br.com.ada.catalogoFilmes.entity.Ator;
+
 public class AtorRepository {
 
+	private List<Ator> atorBanco;
+
+	public AtorRepository() {
+		this.atorBanco = new ArrayList<>();
+	}
+	
+	public void adicionarAtor(Ator ator) {
+		this.atorBanco.add(ator);
+	}
+
+	public void removerAtor(Ator ator) {
+		Iterator<Ator> iterator = atorBanco.iterator();
+		while (iterator.hasNext()) {
+			Ator novo = iterator.next();
+			if (novo.getIdentificador().equals(ator.getIdentificador())) {
+				iterator.remove();
+				break;
+			}
+		}
+	}
+
+	public Ator buscarAtor(Integer identificador) {
+		for (Ator ator : atorBanco) {
+			if (ator.getIdentificador().equals(identificador)) {
+				return ator;
+			}
+		}
+		return null;
+	}
+	
 }
